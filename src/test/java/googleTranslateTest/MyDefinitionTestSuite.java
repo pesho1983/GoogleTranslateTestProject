af11b01c2ffbe0a91c3eee376ccf3c22c73f9639
Petar Aleksandrov <paleksandrov@QH.NET>
2018-06-26 11:57:17 +0300

    delete MyDefinitionTestsuite

--- src/test/java/googleTranslateTest/MyDefinitionTestSuite.java
+++
@@ -1,9 +0,0 @@
-package googleTranslateTest;
-
-import cucumber.api.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/consult_dictionary/googleTranslate.feature")
-public class MyDefinitionTestSuite {}
